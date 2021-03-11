ee8f0e2f924ea9f3f755d93c879ac1c374804e57
Liu, Hao <haoliuhust@hotmail.com>
2021-03-11 11:09:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import os
 import argparse
 
 from models import *
-from utils import progress_bar
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -129,9 +128,6 @@ def test(epoch):
             _, predicted = outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
-
-            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
-                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
     # Save checkpoint.
     acc = 100.*correct/total
